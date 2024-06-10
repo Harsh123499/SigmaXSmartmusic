156fecad1f4d33aad405d4a03f1e4578dbca5682
MR HΛRSH <157881731+Harsh123499@users.noreply.github.com>
2024-06-10 09:18:59 +0530

    Update nexio.py

--- DAXXMUSIC/plugins/sudo/nexio.py
+++ DAXXMUSIC/plugins/sudo/nexio.py
@@ -53,7 +53,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["annu","exi","exiko"], prefixes=["T","t","n", "N"]) & admin_filter)
+@app.on_message(filters.command(["annu"], prefixes=["T","t"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
